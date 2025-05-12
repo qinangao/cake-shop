8b72cd0da3219f2608b4720732542a6104ee8ef3
qinanagao@gmail.com <qinanagao@gmail.com>
2025-05-12 23:07:22 +0100

    implement contact form

--- src/UI/pages/Contact.tsx
+++ src/UI/pages/Contact.tsx
@@ -1,5 +1,78 @@
+import Button from "../components/Button";
+
 function Contact() {
-  return <div className="text-7xl">Contact us</div>;
+  return (
+    <div className="text-[18px] py-10">
+      <h2 className="text-center text-3xl pb-10">
+        Special Requests? We got you! Fill the form to be contacted.
+      </h2>
+      <form className="max-w-lg mx-auto p-6 rounded-xl  space-y-4">
+        <div>
+          <label
+            htmlFor="name"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Name:
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
+          />
+        </div>
+
+        <div>
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Email:
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
+          />
+        </div>
+
+        <div>
+          <label
+            htmlFor="phone"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Phone:
+          </label>
+          <input
+            type="text"
+            id="phone"
+            name="phone"
+            className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
+          />
+        </div>
+
+        <div>
+          <label
+            htmlFor="message"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Message:
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            rows={4}
+            className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
+          ></textarea>
+        </div>
+
+        <div className="flex justify-center">
+          <Button width="300px">Submit</Button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Contact;
