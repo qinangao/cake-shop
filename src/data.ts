4f6c85dce7486d3772fc88dc3c046e7108da9858
qinanagao@gmail.com <qinanagao@gmail.com>
2025-05-17 22:38:23 +0100

    add data

---
+++ src/data.ts
@@ -0,0 +1,83 @@
+// cakesData.ts
+
+export type Cake = {
+  id: number;
+  name: string;
+  price: number;
+  image: string;
+};
+
+export const cakes: Cake[] = [
+  {
+    id: 1,
+    name: "Chocolate Fudge Cake",
+    price: 25.99,
+    image: "/images/chocolate-fudge.jpg",
+  },
+  {
+    id: 2,
+    name: "Red Velvet Cake",
+    price: 29.99,
+    image: "/images/red-velvet.jpg",
+  },
+  {
+    id: 3,
+    name: "Lemon Drizzle Cake",
+    price: 22.5,
+    image: "/images/lemon-drizzle.jpg",
+  },
+  {
+    id: 4,
+    name: "Carrot Cake",
+    price: 24.0,
+    image: "/images/carrot-cake.jpg",
+  },
+  {
+    id: 5,
+    name: "Strawberry Shortcake",
+    price: 27.75,
+    image: "/images/strawberry-shortcake.jpg",
+  },
+  {
+    id: 6,
+    name: "Vanilla Bean Cake",
+    price: 21.95,
+    image: "/images/vanilla-bean.jpg",
+  },
+  {
+    id: 7,
+    name: "Black Forest Cake",
+    price: 30.0,
+    image: "/images/black-forest.jpg",
+  },
+  {
+    id: 8,
+    name: "Coffee Walnut Cake",
+    price: 23.5,
+    image: "/images/coffee-walnut.jpg",
+  },
+  {
+    id: 9,
+    name: "Pineapple Upside Down Cake",
+    price: 26.25,
+    image: "/images/pineapple-upside.jpg",
+  },
+  {
+    id: 10,
+    name: "Funfetti Cake",
+    price: 20.99,
+    image: "/images/funfetti.jpg",
+  },
+  {
+    id: 11,
+    name: "Tiramisu Cake",
+    price: 31.5,
+    image: "/images/tiramisu.jpg",
+  },
+  {
+    id: 12,
+    name: "Coconut Cream Cake",
+    price: 28.0,
+    image: "/images/coconut-cream.jpg",
+  },
+];
