44ba91979d3be43a6457937898c8574d1dcbb51e
qinanagao@gmail.com <qinanagao@gmail.com>
2025-05-17 22:38:34 +0100

    feat: update Menu and MenuItem components to display cake data dynamically

--- src/features/menu/Menu.tsx
+++ src/features/menu/Menu.tsx
@@ -1,12 +1,16 @@
+import { cakes } from "../../data.ts";
 import MenuItem from "./MenuItem";
+
 function Menu() {
   return (
-    <div className="py-10">
-      <h2 className="text-4xl font-semibold text-center">Our Cakes</h2>
-      <div>
-        <MenuItem />
+    <section className="py-10">
+      <h2 className="text-4xl font-semibold text-center mb-8">Our Cakes</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center">
+        {cakes.map((cake) => (
+          <MenuItem key={cake.id} cake={cake} />
+        ))}
       </div>
-    </div>
+    </section>
   );
 }
 
--- src/features/menu/MenuItem.tsx
+++ src/features/menu/MenuItem.tsx
@@ -1,12 +1,18 @@
 import Button from "../../UI/components/Button";
-function MenuItem() {
+import { Cake } from "../../data.ts";
+
+type MenuItemProps = {
+  cake: Cake;
+};
+
+function MenuItem({ cake }: MenuItemProps) {
   return (
     <div className="w-[300px] border-1 border-gray-200 px-4 py-6 rounded-[20px]">
       <img src="/assets/cakes/image-tiramisu-desktop.jpg" alt="" />
       <div className="flex items-center justify-between pt-4">
         <div>
-          <p>Classic Tiramisu</p>
-          <p className="font-semibold">£5.5</p>
+          <p>{cake.name}</p>
+          <p className="font-semibold">£{cake.price.toFixed(2)}</p>
         </div>
         <Button>Add</Button>
       </div>
