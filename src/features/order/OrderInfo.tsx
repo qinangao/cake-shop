1c01a640bc26adf10304cf6f86d8785cb305557a
qinanagao@gmail.com <qinanagao@gmail.com>
2025-05-26 18:10:51 +0100

    add OrderInfo component to display order confirmation details

---
+++ src/features/order/OrderInfo.tsx
@@ -0,0 +1,62 @@
+import { useLocation } from "react-router-dom";
+import { formatNiceDate, generateShortOrderId } from "../../utilities/helper";
+import { useCustomerInfo } from "./useCustomerInfo";
+import OrderItemRow from "./OrderItemRow";
+import { CartItem } from "../cart/cartSlice";
+
+function OrderInfo() {
+  const { collectDate, name, email } = useCustomerInfo();
+  const orderId = generateShortOrderId();
+
+  const location = useLocation();
+  const { orderItems, total }: { orderItems: CartItem[]; total: number } =
+    location.state || {
+      orderItems: [],
+      total: 0,
+    };
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className="text-center pt-10">
+        <div className="flex items-center justify-center gap-2">
+          <img src="/assets/icons/icons8-approve.svg" alt="" />{" "}
+          <h1 className="text-3xl font-bold">Your Order Is Confirmed</h1>
+        </div>
+
+        <div className="text-lg py-4 leading-[40px]">
+          <p>
+            Order number: <strong>{orderId}</strong>
+          </p>
+          <p>
+            Thank you {name}! Your order has been successfully placed! A
+            confirmation email has sent to <strong>{email}</strong>
+          </p>
+          <p>
+            It will be ready for collection on
+            <strong>
+              {" "}
+              {collectDate ? formatNiceDate(collectDate) : "N/A"}
+            </strong>{" "}
+            at store.
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-col rounded-2xl justify-center w-[40%] border-1 border-gray-300 py-4 px-8 my-6">
+        <h2 className="text-2xl">Order summary</h2>
+        <ul className=" px-3 my-4">
+          {orderItems.map((item) => (
+            <OrderItemRow
+              item={item}
+              key={`${item.name}-${item.size}-${item.quantity}`}
+            />
+          ))}
+        </ul>
+        <div className="text-2xl flex justify-between">
+          <p>Total: </p>
+          <p>${total}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default OrderInfo;
