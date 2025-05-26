54762ef24b39e2c7da01027fcb5c27ba3cb1b3d8
qinanagao@gmail.com <qinanagao@gmail.com>
2025-05-26 18:11:04 +0100

    add OrderConfirmation component to display order details and additional information

---
+++ src/UI/pages/OrderConfirmation.tsx
@@ -0,0 +1,13 @@
+import VisitUs from "../components/VisitUs";
+import OrderInfo from "../../features/order/OrderInfo";
+
+function OrderConfirmation() {
+  return (
+    <div>
+      <OrderInfo />
+      <VisitUs />
+    </div>
+  );
+}
+
+export default OrderConfirmation;
