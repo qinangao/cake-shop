9e3c17f86a24ffb0b452a61a4c1225e0dfe67519
qinanagao@gmail.com <qinanagao@gmail.com>
2025-05-26 18:10:58 +0100

    refactor OrderSummary to integrate order placement logic and manage cart state

--- src/features/order/OrderSummary.tsx
+++ src/features/order/OrderSummary.tsx
@@ -1,13 +1,28 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Button from "../../UI/components/Button";
 import OrderItemRow from "./OrderItemRow";
-import { getCart, getTotalCartPrice } from "../cart/cartSlice";
+import { clearCart, getCart, getTotalCartPrice } from "../cart/cartSlice";
+import { useNavigate } from "react-router-dom";
+import { useCustomerInfo } from "./useCustomerInfo";
 
 function OrderSummary() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const cart = useSelector(getCart);
   const totalCartPrice = useSelector(getTotalCartPrice);
+  const { isFilledInfo } = useCustomerInfo();
+  console.log(isFilledInfo);
 
-  console.log(cart);
+  function handlePlaceOrder() {
+    navigate("/confirmation", {
+      state: {
+        orderItems: cart,
+        total: totalCartPrice,
+      },
+    });
+    dispatch(clearCart());
+  }
+
   return (
     <div className="p-6 bg-gray-50 rounded-lg shadow-md flex flex-col">
       <h2 className="text-2xl font-semibold mb-6 text-center md:text-left">
@@ -31,7 +46,13 @@ function OrderSummary() {
           </div>
 
           <div className="mt-auto">
-            <Button width="100%" bgColor="black" textColor="white">
+            <Button
+              width="100%"
+              bgColor="black"
+              textColor="white"
+              onClick={handlePlaceOrder}
+              disabled={!isFilledInfo}
+            >
               Place Order
             </Button>
           </div>
