196c14b8165aa3f05b8acc59183e606e1b67ce04
qinanagao@gmail.com <qinanagao@gmail.com>
2025-05-28 22:37:31 +0100

    create itemDetail hook for item in cart

---
+++ src/hooks/useItemDetail.ts
@@ -0,0 +1,76 @@
+import { useDispatch } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+import { Cake, cakes, CakeSize } from "../data";
+import { useState } from "react";
+import { setAutoOpenCart } from "../utilities/cartSideBarControl";
+import { addItem } from "../features/cart/cartSlice";
+
+export type ItemDetailHook = {
+  handleAddtoCart: () => void;
+  handleOrderNow: () => void;
+  setSelectSize: React.Dispatch<React.SetStateAction<CakeSize | "">>;
+  setQuantity: React.Dispatch<React.SetStateAction<number>>;
+  cake: Cake;
+  quantity: number;
+  selectSize: CakeSize | "";
+};
+
+export function useItemDetail(): ItemDetailHook | undefined {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const { id } = useParams<{ id: string }>();
+  const cake = cakes.find((c) => c.id === Number(id));
+
+  const [selectSize, setSelectSize] = useState<CakeSize | "">("");
+  const [quantity, setQuantity] = useState<number>(1);
+
+  if (!cake) return;
+
+  const priceMultiplier = selectSize === "8-inch" ? 1.5 : 1;
+  const basePrice = cake.unitPrice * priceMultiplier;
+
+  const totalPrice = (quantity ?? 0) * basePrice;
+
+  function handleAddtoCart() {
+    if (!selectSize || !quantity) return;
+    setAutoOpenCart(true);
+    const newItem = {
+      cakeId: cake?.id,
+      name: cake?.name,
+      size: selectSize as CakeSize,
+      quantity,
+      basePrice,
+      totalPrice,
+    };
+    console.log(newItem);
+    dispatch(addItem(newItem));
+  }
+
+  function handleOrderNow() {
+    if (!selectSize || !quantity) return;
+    setAutoOpenCart(false);
+    const newItem = {
+      cakeId: cake?.id,
+      name: cake?.name,
+      size: selectSize as CakeSize,
+      quantity,
+      basePrice,
+      totalPrice,
+    };
+
+    dispatch(addItem(newItem));
+    navigate("/checkout");
+  }
+  return {
+    handleAddtoCart,
+    handleOrderNow,
+    setSelectSize,
+    setQuantity,
+    cake,
+    quantity,
+    selectSize,
+  };
+}
+
+export default useItemDetail;
